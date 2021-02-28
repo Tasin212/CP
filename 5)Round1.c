0ecc59764b4b29e1636013d89301df887199df6a
Tasin212 <79746688+Tasin212@users.noreply.github.com>
2021-02-28 16:17:51 +0600

    Puzzles Tric Round1 by AtCoding
    It took me two attempts the first attempt was successful based on the sample input but 3 unseen samples didn't pass out.

---
+++ 5)Round1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    int A,B;
+    scanf("%d %d",&A,&B);
+    int x=6-A-B;
+    printf("%d",x);
+      
+    return 0;
+}
